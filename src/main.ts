975fa4f89c711a60d4cef8f2f2e033d9a9052464
Graham Rogers <gmrogers04@gmail.com>
2022-04-09 08:32:46 -0700

    :gem: set up prisma example

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     await app.listen(3000);
+
+    // Prisma interferes with NestJS `enableShutdownHooks`
+    // see https://docs.nestjs.com/recipes/prisma#issues-with-enableshutdownhooks
+    const prismaService = app.get(PrismaService);
+    await prismaService.enableShutdownHooks(app);
 }
 bootstrap();
---
+++ src/post/post.controller.ts
@@ -0,0 +1,60 @@
+import { Controller, Get, Post, Put, Body, Patch, Param, Delete } from '@nestjs/common';
+import { PostService } from './post.service';
+
+import { Post as PostModel } from '@prisma/client';
+
+@Controller('posts')
+export class PostController {
+    constructor(private readonly postService: PostService) {}
+
+    @Post('post')
+    async createDraft(@Body() postData: { title: string; content?: string; authorEmail: string }): Promise<PostModel> {
+        const { title, content, authorEmail } = postData;
+        return this.postService.create({
+            title,
+            content,
+            author: {
+                connect: { email: authorEmail },
+            },
+        });
+    }
+
+    @Get(':id')
+    async getPostById(@Param('id') id: string): Promise<PostModel> {
+        return this.postService.findOne({ id: Number(id) });
+    }
+
+    @Get('feed')
+    async getPublishedPosts(): Promise<PostModel[]> {
+        return this.postService.findAll({ where: { published: true } });
+    }
+
+    @Get('filtered/:searchString')
+    async getFilteredPosts(@Param('searchString') searchString: string): Promise<PostModel[]> {
+        return this.postService.findAll({
+            where: {
+                OR: [
+                    {
+                        title: { contains: searchString },
+                    },
+                    {
+                        content: { contains: searchString },
+                    },
+                ],
+            },
+        });
+    }
+
+    @Put(':id/publishw ')
+    async publishPost(@Param('id') id: string): Promise<PostModel> {
+        return this.postService.update({
+            where: { id: Number(id) },
+            data: { published: true },
+        });
+    }
+
+    @Delete(':id')
+    async deletePost(@Param('id') id: string): Promise<PostModel> {
+        return this.postService.remove({ id: Number(id) });
+    }
+}
